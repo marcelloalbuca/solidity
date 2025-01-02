bea4a561e3f8222ef621832c8f1c96a1baf0391c
marcelloalbuca <marcello.albuca@gmail.com>
2025-01-02 17:19:17 -0300

    example int

---
+++ int.sol
@@ -0,0 +1,12 @@
+As seguintes operações podem ser aplicadas a inteiros:
+
+Comparações: <=, <, ==, !=, >=, >
+Operadores de bits: & (and), | (or), ^ (bitwise exclusive), ~ (bitwise negation)
+Operadores aritméticos: + (addition),- (subtraction), * (multiplication), / (division), % (modulo), ** (exponential)
+
+int32 price = 25; // signed 32 bit integer
+uint256 balance = 1000; // unsigned 256 bit integer
+
+balance - price; // 975
+2 * price; // 50
+price % 2; // 1
