aa514c370f6ee882a65f23c4c75cc32649a8f305
marcelloalbuca <marcello.albuca@gmail.com>
2025-01-02 17:23:30 -0300

    example string

---
+++ string.sol
@@ -0,0 +1,16 @@
+Literais de cadeia de caracteres
+String literals também são usados na maioria dos arquivos de contrato. 
+Eles são caracteres ou palavras entre aspas duplas ou simples.
+
+
+String shipped = "shipped"; // shipped
+    String delivered = 'delivered'; // delivered
+    String newItem = "newItem"; // newItem
+
+
+Além disso, os seguintes caracteres de escape podem ser usados com string literals.
+
+\<newline> ignora uma nova linha
+\n nova linha
+\r retorno de carro
+\t tabulação
