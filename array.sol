9b5a41be28324a458cdc94c97aa6422e25aa64ed
marcelloalbuca <marcello.albuca@gmail.com>
2025-01-06 10:07:54 -0300

    example array

---
+++ array.sol
@@ -0,0 +1,15 @@
+uint[] itemIds; // Declare a dynamically sized array called itemIds
+uint[3] prices = [1, 2, 3]; // initialize a fixed size array called prices, with prices 1, 2, and 3
+uint[] prices = [1, 2, 3]; // same as above
+
+
+As matrizes podem ter um tamanho fixo ou dinâmico. Seus índices começam em 0.
+
+Para criar uma matriz de k de tamanho fixo e tipo de elemento T, você escreveria T[k]. Para uma matriz de dimensionamento dinâmico, você escreveria T[] .
+
+Elementos de matriz podem ser de qualquer tipo. Por exemplo, podem conter uint, memória ou bytes. As matrizes também podem incluir mapeamentos ou structs.
+
+
+multidimensional_array
+
+
