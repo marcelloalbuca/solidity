d047c3c6637867eb1d55b3ee8e9fec64a3bbab2d
marcelloalbuca <marcello.albuca@gmail.com>
2025-01-06 10:10:38 -0300

    example structs estrutura

---
+++ structs.sol
@@ -0,0 +1,11 @@
+struct Items_Schema {
+    uint256 _id;
+    uint256 _price;
+    string _name;
+    string _description;
+}
+
+
+estruturas
+Structs são tipos personalizados que um usuário pode definir para representar 
+bjetos do mundo real. Normalmente, structs são usados como um esquema ou para representar registros.
