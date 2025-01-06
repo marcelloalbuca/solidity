ecee65f2c25a147a1c84cd760fe70906cf43beca
marcelloalbuca <marcello.albuca@gmail.com>
2025-01-06 10:08:59 -0300

    array functions push, pop, length

---
+++ arraymember.sol
@@ -0,0 +1,11 @@
+// Create a dynamic byte array
+bytes32[] itemNames;
+itemNames.push(bytes32("computer")); // adds "computer" to the array
+itemNames.length; // 1
+
+
+Os seguintes membros podem manipular e obter informações sobre matrizes:
+
+length: obter o comprimento de uma matriz.
+push(): acrescentar um elemento no final da matriz.
+pop: remover um elemento do final de uma matriz.
